4e8112f16d1fbc5ca3e18e310e398aa3dca2e76f
dbillings <dbillings@rumblefish.net>
2015-12-15 15:29:46 -0800

    more generizing, remove some dead code

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,13 +7,12 @@ export LANGUAGE=en_US.UTF-8
 export LANG=en_US.UTF-8
 export LC_ALL=en_US.UTF-8
 locale-gen en_US.UTF-8
-
 # Add Repository
 sudo apt-get -y -q install python-software-properties
 sudo add-apt-repository ppa:pi-rho/dev
 sudo apt-get update
 
-# install basic unix packages
+install basic unix packages
 sudo apt-get install -y -q \
   bash-completion \
   build-essential \
@@ -33,26 +32,29 @@ sudo apt-get install -y -q \
   vim \
   wget
 
-#configure app env
+# configure app env
 su -c 'sudo mkdir /var/www && sudo chmod 777 -R /var/www;' vagrant
 su -c 'sudo ln -s /var/www ~/www;' vagrant
 
-#remove vagrant default ruby and install rvm
+# remove vagrant default ruby and install rvm
 su -c 'sudo rm -rf /opt/vagrant_ruby/ && \curl -sSL https://get.rvm.io | bash -s stable --rails --ruby=2.1.1;' vagrant
 
 
-#install ruby deps for bundle
+# install ruby deps for bundle
 sudo apt-get install -y -q \
 ruby-dev \
 libsqlite3-dev
 
-#install bundle
+# install bundle
 sudo gem install bundle
 
-#pssh / portly
+# pssh / portly
 sudo gem install pssh
 
-#debug
+# debug
 su -c 'pwd;' vagrant
 
-exit
+###########################
+# BOOTSTRAP THE PROJECT #
+# edit for project specific build     #
+###########################
